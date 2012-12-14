9bd46461e682898f5c475e599a7409fdfef3065b
ladenree <olivier@cavaweb.info>
2012-12-14 18:16:33 +0100

    Indent Conde

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
+
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 $app->config(array('templates.path' => './templates'));
 
-$app->get('/', function() use($app){
-	$app->render('index.php');
-});
+$app->get('/', function() use($app) {
+            $app->render('index.php');
+        });
 
-$app->get('/admin', function() use($app){
-	$app->render('admin/index.html');
-});
-$app->get('/graph/temperature', function() use($app){
-	$app->render('gtemperature.php');
-});
+$app->get('/admin', function() use($app) {
+            $app->render('admin/index.html');
+        });
+$app->get('/graph/temperature', function() use($app) {
+            $app->render('gtemperature.php');
+        });
 
 $app->run();
 ?>
